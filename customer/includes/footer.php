a8fb5bb7f12abd13ba7866d6925750d99d8c3414
pritipadmamishra <pritipadmamishra38@gmail.com>
2021-11-11 15:37:32 +0530

    My Account Added

---
+++ customer/includes/footer.php
@@ -0,0 +1,153 @@
+<div id="footer"><!-- footer Starts -->
+<div class="container"><!-- container Starts -->
+
+<div class="row" ><!-- row Starts -->
+
+<div class="col-md-3 col-sm-6" ><!-- col-md-3 col-sm-6 Starts -->
+
+<h4>Pages</h4>
+
+<ul><!-- ul Starts -->
+
+<li><a href="../cart.php">Shopping Cart</a></li>
+
+<li><a href="../contact.php">Contact Us</a></li>
+
+<li><a href="../shop.php">Shop</a></li>
+
+<li><a href="my_account.php">My Account</a></li>
+
+</li>
+
+
+</ul><!-- ul Ends -->
+
+<hr>
+
+<h4>User Section</h4>
+
+<ul><!-- ul Starts -->
+
+<li><a href="../checkout.php">Login</a></li>
+
+<li><a href="../customer_register.php">Register</a></li>
+
+<li><a href="terms.php">Terms And Conditions </a></li>
+
+
+
+</ul><!-- ul Ends -->
+
+<hr class="hidden-md hidden-lg hidden-sm" >
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4> Top Products Categories </h4>
+
+<ul><!-- ul Starts -->
+    <li><a href="#">Jackets</a></li>
+    <li><a href="#">Accesories</a></li>
+    <li><a href="#">Shoes</a></li>
+    <li><a href="#">Coats</a></li>
+    <li><a href="#">T-Shirts</a></li>
+</ul><!-- ul Ends -->
+
+<hr class="hidden-md hidden-lg">
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4>Where to find us</h4>
+
+<p><!-- p Starts -->
+<strong>E-Code-Commerce Ltd.</strong>
+<br>Code Plus Plus Community
+<br>Outr, Bhubaneswar
+<br>9999999999
+<br>codeplusplus@gmail.com
+<br>
+<strong>Developers- Priti and Debi</strong>
+
+</p><!-- p Ends -->
+
+<a href="../contact.php">Go to Contact us page</a>
+
+<hr class="hidden-md hidden-lg">
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4>Get the news</h4>
+
+<p class="text-muted">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae repellendus modi atque. Fugit alias voluptates, inventore minus temporibus magnam quidem impedit velit mollitia accusamus, debitis vitae consectetur? Voluptatum, beatae aut.
+</p>
+
+<form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=computerfever', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true"><!-- form Starts -->
+
+<div class="input-group"><!-- input-group Starts -->
+
+<input type="text" class="form-control" name="email">
+
+<input type="hidden" value="computerfever" name="uri"/>
+<input type="hidden" name="loc" value="en_US"/>
+
+<span class="input-group-btn"><!-- input-group-btn Starts -->
+
+<input type="submit" value="Subscribe" class="btn btn-default">
+
+</span><!-- input-group-btn Ends -->
+
+</div><!-- input-group Ends -->
+
+</form><!-- form Ends -->
+
+<hr>
+
+<h4> Stay in touch </h4>
+
+<p class="social"><!-- social Starts --->
+
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-instagram"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+<a href="#"><i class="fa fa-envelope"></i></a>
+
+</p><!-- social Ends --->
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+</div><!-- row Ends -->
+
+</div><!-- container Ends -->
+</div><!-- footer Ends -->
+
+<div id="copyright"><!-- copyright Starts -->
+
+<div class="container" ><!-- container Starts -->
+
+<div class="col-md-6" ><!-- col-md-6 Starts -->
+
+<p class="pull-left"> &copy; Inspiration- Mohammed Tahir Ahmed </p>
+
+</div><!-- col-md-6 Ends -->
+
+<div class="col-md-6" ><!-- col-md-6 Starts -->
+
+<p class="pull-right" >
+
+Ispration From- <a href="http://www.computerfever.com" >Computerfever.com</a>
+
+</p>
+
+</div><!-- col-md-6 Ends -->
+
+</div><!-- container Ends -->
+
+</div><!-- copyright Ends -->
--- index.php
+++ index.php
@@ -73,7 +73,7 @@ echo "";
                         <a href="shop.php">Shop</a>
                     </li>
                     <li>
-                        <a href="checkout.php">My Account</a>
+                        <a href="customer/my_account.php">My Account</a>
                     </li>
                     <li>
                         <a href="cart.php">Shopping Cart</a>
