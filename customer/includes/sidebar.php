816c86294a397f16a5091976e4bc8fe96a5d35da
PRITIPADMA <pritipadmamishra38@gmail.com>
2021-11-13 20:05:46 +0530

    account updated

--- customer/includes/sidebar.php
+++ customer/includes/sidebar.php
@@ -8,8 +8,28 @@
     </div>
     <div class"panal-body">
         <ul class="nav nav-pills nav-stacked">
-            <li>
+            <li class="<?php if(isset($_GET['my_orders'])) {echo "active";}?>">
                 <a href="my_account.php?my_orders"><i class="fa fa-list"></i>My orders
+            </a>
+            </li>
+            <li class="<?php if(isset($_GET['pay_offline'])) {echo "active";}?>">
+                <a href="my_account.php?pay_offline"><i class="fa fa-bolt"></i>pay offline
+            </a>
+            </li>
+            <li class="<?php if(isset($_GET['edit_account'])) {echo "active";}?>">
+                <a href="my_account.php?pay_offline"><i class="fa fa-pencil"></i>Edit Account
+            </a>
+            </li>
+            <li class="<?php if(isset($_GET['change_pass'])) {echo "active";}?>">
+                <a href="my_account.php?pay_offline"><i class="fa fa-user"></i>change password
+            </a>
+            </li>
+            <li class="<?php if(isset($_GET['delete_account'])) {echo "active";}?>">
+                <a href="my_account.php?delete_account"><i class="fa fa-trash-o"></i>Delete Account
+            </a>
+            </li>
+            <li>
+                <a href="logout.php"><i class="fa fa-sign-out"></i>logout
             </a>
             </li>
         </ul>
